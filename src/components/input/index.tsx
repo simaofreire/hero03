9f1a84dee692fb53a7d9b5df837c2f5f4115935f
Simão Freire <simaofreire@live.com>
2024-01-30 00:02:49 -0300

    feat: initial structure

---
+++ src/components/input/index.tsx
@@ -0,0 +1,21 @@
+import { ForwardRefRenderFunction, forwardRef } from 'react';
+
+interface IInput {
+	placeholder: string;
+	type: string;
+}
+
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, IInput> = ({ placeholder, type, ...rest }, ref) => {
+	return (
+		<div className="w-full">
+			<input
+				type={type}
+				placeholder={placeholder}
+				{...rest}
+				ref={ref}
+				className="px-3 py-2 bg-gray-950 rounded-md w-full"
+			/>
+		</div>
+	);
+};
+export const Input = forwardRef(InputBase);
